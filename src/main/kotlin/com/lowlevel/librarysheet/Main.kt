3a807b98fb93c76dc51ec44c4b55df7e46a7fbe4
Linuccia <polina.stephanskaya@gmail.com>
2022-01-10 15:30:08 +0300

    1.0.1

--- src/main/kotlin/com/lowlevel/librarysheet/Main.kt
+++ src/main/kotlin/com/lowlevel/librarysheet/Main.kt
@@ -5,6 +5,6 @@ import com.lowlevel.librarysheet.view.UIApp
 import tornadofx.launch
 
 fun main() {
-    DatabaseConnection.connect("C:\\Study\\low-level\\LibrarySheet\\src\\main\\resources\\database.properties")
+    DatabaseConnection.connect("src/main/resources/database.properties")
     launch<UIApp>()
 }
